b21feac22cf3b45ce4a773ad2746c286d107df98
arman <arman.saftalyan@gmail.com>
2023-05-02 18:13:27 +0400

    route to dev.exe

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,7 +4,7 @@
   <ul>
     <li><nuxt-link to="/about">About</nuxt-link></li>
     <li><button @click="$root.$emit('toggleModal', {target: 'help'})" type="button">Help</button></li>
-    <li><nuxt-link to="/">For Developers</nuxt-link></li>
+    <li><a href="https://dev.exe.world" target="_blank">For Developers</a></li>
   </ul>
 </footer>
 </template>
