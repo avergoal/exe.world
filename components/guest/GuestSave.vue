c14a5ae1a65c52ca611fa1a65c7fa5b59e751994
arman <arman.saftalyan@gmail.com>
2023-12-27 02:18:55 +0400

    fixes 26.12.2023

--- components/guest/GuestSave.vue
+++ components/guest/GuestSave.vue
@@ -7,7 +7,7 @@
     <img class="guest-save__img" src="~/assets/illustration/sleek.svg" alt="">
     <div class="guest-save-text">
       <div class="guest-save__title">Save your game progress!</div>
-      <div class="guest-save__desc">Register to save your progress and get 10 bonuses to your account</div>
+      <div class="guest-save__desc">Sign up to save your game progress</div>
     </div>
     <div class="guest-save-btns">
       <button @click="$root.$emit('toggleModal', {target: 'signUp'})" type="button" class="btn st2">
