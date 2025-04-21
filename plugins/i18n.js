3b0c2429c0a4ac23513f70fa1e6abb8908d89c77
arman <arman.saftalyan@gmail.com>
2025-04-21 14:52:29 +0400

    lang fix

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,16 +1,19 @@
 export default async ({app, store}) => {
-  const {data} = await store.dispatch('app/getTranslation');
-  const translations = data?.response;
+  if (!process.client) return;
+  setTimeout(async ()=>{
+    const {data} = await store.dispatch('app/getTranslation');
+    const translations = data?.response;
 
-  const locales = store.getters['app/locales'];
-  app.i18n.locales = [
-    { code: 'en', iso: 'en-US', file: 'en.js' },
-    { code: 'ru', iso: 'ru-RU', file: 'ru.js' }
-  ]
-  app.i18n.localeCodes = locales.map(locale => locale.code)
-  app.i18n.defaultLocale = 'en'
-  app.i18n.fallbackLocale = 'en'
-  app.i18n.setLocaleMessage('en', translations);
+    const locales = store.getters['app/locales'];
+    app.i18n.locales = [
+      { code: 'en', iso: 'en-US', file: 'en.js' },
+      { code: 'ru', iso: 'ru-RU', file: 'ru.js' }
+    ]
+    app.i18n.localeCodes = locales.map(locale => locale.code)
+    app.i18n.defaultLocale = 'en'
+    app.i18n.fallbackLocale = 'en'
+    app.i18n.setLocaleMessage('en', translations);
+  },10)
 };
 
 
